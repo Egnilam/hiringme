ebd2c9c472bfc0ad87896d8c18aad8c5f4d6e589
Egnilam <malinge.florian@gmail.com>
2023-10-24 15:45:45 +0200

    [WIP][#10] - application http

---
+++ src/Application/Http/Wishlist/WishlistGroup/WishlistGroupListController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Http\Wishlist\WishlistGroup;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('wishlist_groups')]
+final class WishlistGroupListController extends AbstractController
+{
+    #[Route(name: 'wishlist_group_list', methods: ['GET'])]
+    public function __invoke(Request $request): Response
+    {
+        return $this->render('wishlist/wishlist_group/list.html.twig');
+    }
+}
