d0777a642ac328c99dfdb765d5b537978edd7cdf
Egnilam <malinge.florian@gmail.com>
2023-11-16 15:56:11 +0100

    [WIP] - start app design

--- src/Application/Http/Wishlist/WishlistGroup/WishlistGroupMember/AssociateWishlistToGroupMemberController.php
+++ src/Application/Http/Wishlist/WishlistGroup/WishlistGroupMember/AssociateWishlistToGroupMemberController.php
@@ -10,6 +10,7 @@ use App\Application\Form\Wishlist\WishlistGroup\WishlistGroupMember\AssociateWis
 use App\Infrastructure\Framework\Messenger\Command\CommandBusInterface;
 use App\Infrastructure\Framework\Messenger\Query\QueryBusInterface;
 use Domain\Wishlist\Request\Query\GetListWishlistRequest;
+use Domain\Wishlist\Response\WishlistResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,18 +20,30 @@ use Symfony\Component\Routing\Annotation\Route;
 final class AssociateWishlistToGroupMemberController extends AbstractController
 {
     #[Route('/{id}/wishlist', name: 'wishlist_group_member_associate_wishlist', methods: ['GET', 'POST'])]
-    public function __invoke(Request $request, string $groupId, string $id, QueryBusInterface $queryBus, CommandBusInterface $commandBus): Response {
+    public function __invoke(Request $request, string $groupId, string $id, QueryBusInterface $queryBus, CommandBusInterface $commandBus): Response
+    {
 
         $command = new AssociateGroupMemberToWishlistCommand();
         $command->setWishlistGroupId($groupId);
 
         $query = new GetListWishlistQuery();
+
+        /** @var array<WishlistResponse> $wishlists */
         $wishlists = $queryBus->ask($query->setRequest(new GetListWishlistRequest($id)));
+        if(count($wishlists) === 0) {
+            return $this->redirectToRoute('wishlist_create', ['group' => $groupId]);
+        }
 
         $form = $this->createForm(AssociateWishlistToGroupMemberForm::class, $command, ['wishlists' => $wishlists]);
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-            $commandBus->dispatch($command);
+        if($form->isSubmitted() && $form->isValid()) {
+            try {
+                $commandBus->dispatch($command);
+
+                return $this->redirectToRoute('wishlist_group_show', ['id' => $groupId]);
+            } catch (\Exception $exception) {
+
+            }
         }
 
         return $this->render('wishlist/wishlist_group/wishlist_group_member/associate_wishlist.html.twig', [
